0055e5e1987e226eda1b8e321af271a4aac3d8a6
Arian <arian.kulmer@web.de>
2022-08-28 17:58:43 +0200

    nicole: Copy powerhint.xml from power HAL
    Change-Id: I6f8ca59f4ad0ed7324010f657475d020fa76cb2f

--- device.mk
+++ device.mk
@@ -243,6 +243,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 $(call soong_config_set,qtipower,tap_to_wake_node,/sys/devices/platform/soc/a90000.spi/spi_master/spi0/spi0.0/fts_gesture_mode)
 
 # Public libraries

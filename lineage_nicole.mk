fe45a3f5a5fde858c76f0f0f05bfd0d5de957eda
Balázs Triszka <info@balika011.hu>
2025-12-07 18:12:08 +0100

    nicole: Add libinit_nicole
    * Needed in both OS and recovery for serial number set.
       * `setsn` binary does this on stock.
      * Need to mount persist from in init to fetch the data,
        as mounting via fstab results in it being set too late.
    
    * Rely on kernel-passed SKU property to tell if device is 5G
      or Wi-Fi-Only, and set appropriate model/name/device/fingerprint.
    
    Change-Id: I1af7285e40f750ad28f0b86c9302e69b2dfc30b5

--- lineage_nicole.mk
+++ lineage_nicole.mk
@@ -27,7 +27,3 @@ PRODUCT_MANUFACTURER := Razer
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_GMS_CLIENTID_BASE := android-huaqin
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Razer/Nicole/Razer-Edge-WiFi:12/SKQ1.211103.001/172:user/release-keys \
-    DeviceProduct=nicole

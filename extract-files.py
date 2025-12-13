2d5cf1522d75fb21a2b72c1a05f38e94c2945c24
Michael Bestas <mkbestas@lineageos.org>
2025-12-13 06:10:59 +0200

    nicole: Conditionally enable rild for 5g model
    Change-Id: I6acc49cd7df11eaa8bd02d32767eb1bda2f66fb9

--- extract-files.py
+++ extract-files.py
@@ -43,6 +43,9 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/android.hardware.radio@1.4-service.quectel.rc': blob_fixup()
+        .regex_replace(' quectel.ril-daemon', ' vendor.quectel.ril-daemon')
+        .add_line_if_missing('    disabled'),
     'vendor/etc/init/imsdaemon.rc': blob_fixup()
         .add_line_if_missing('    disabled'),
     'vendor/lib64/hw/libquectel-ril.so': blob_fixup()

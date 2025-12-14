eacf2f67808e44b7a86406c5b922f9a4b3b89402
Michael Bestas <mkbestas@lineageos.org>
2025-12-14 03:35:55 +0200

    nicole: Mark android.hardware.camera feature as unavailable
    Change-Id: I7f05eb84419645dccbb631a24890496f7672d55e

--- device.mk
+++ device.mk
@@ -99,6 +99,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/unavail.android.hardware.camera.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/unavail.android.hardware.camera.xml
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 

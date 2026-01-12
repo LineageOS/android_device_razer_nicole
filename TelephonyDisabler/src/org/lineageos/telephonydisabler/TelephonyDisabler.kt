5fba2529afdba78f34dfe519e4d881a7f87fb4e7
AnierinB <anierin@evolution-x.org>
2026-01-12 08:18:40 +0000

    nicole: TelephonyDisabler: Drop Euicc handling & migrate to hasSystemFeature
    Change-Id: I2fa2957cedc4badc9ada5c542080f60e59cb789f
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    (cherry picked from commit 9fdd82e36607c875ce2bd63d87c872f6b9c70412)
    (cherry picked from commit 4ebbae3baa24307706707065f0b2c73a77030d02)

--- TelephonyDisabler/src/org/lineageos/telephonydisabler/TelephonyDisabler.kt
+++ TelephonyDisabler/src/org/lineageos/telephonydisabler/TelephonyDisabler.kt
@@ -9,25 +9,12 @@ import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.pm.PackageManager.ApplicationInfoFlags
-import android.os.Build
 import android.util.Log
 
 object TelephonyDisabler {
     private const val TAG = "TelephonyDisabler"
 
-    private const val SKU_WIFI = "wifi"
-
-    private val EUICC_DEPENDENCIES = listOf(
-        "com.google.android.gms",
-        "com.google.android.gsf",
-    )
-
-    private val EUICC_PACKAGES = listOf(
-        "com.google.android.euicc",
-        "com.google.android.ims",
-    )
-
-    private val ADDITIONAL_PACKAGES = listOf(
+    private val TELEPHONY_PACKAGES = listOf(
         "com.android.messaging",
         "com.android.ons",
         "com.google.android.messages",
@@ -37,36 +24,26 @@ object TelephonyDisabler {
         "com.vzw.apnlib",
     )
 
-    private fun isInstalled(pm: PackageManager, pkgName: String) = runCatching {
-        val info = pm.getApplicationInfo(pkgName, ApplicationInfoFlags.of(0))
-        info.flags and ApplicationInfo.FLAG_INSTALLED != 0
-    }.getOrDefault(false)
-
-    private fun isInstalledAndEnabled(pm: PackageManager, pkgName: String) = runCatching {
-        val info = pm.getApplicationInfo(pkgName, ApplicationInfoFlags.of(0))
-        Log.d(TAG, "package $pkgName installed, enabled = ${info.enabled}")
-        info.enabled
-    }.getOrDefault(false)
+    private fun isInstalled(pm: PackageManager, pkgName: String) =
+        runCatching {
+                val info = pm.getApplicationInfo(pkgName, ApplicationInfoFlags.of(0))
+                info.flags and ApplicationInfo.FLAG_INSTALLED != 0
+            }
+            .getOrDefault(false)
 
     fun enableOrDisablePackages(context: Context) {
         val pm = context.packageManager
-        val isWifi = Build.ODM_SKU == SKU_WIFI
-        val gmsMissing = EUICC_DEPENDENCIES.any { !isInstalledAndEnabled(pm, it) }
-
-        mapOf(
-            EUICC_PACKAGES to (isWifi || gmsMissing),
-            ADDITIONAL_PACKAGES to isWifi
-        ).forEach { (pkgs, shouldDisable) ->
-            val flag = if (shouldDisable) {
+        val disable = !pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
+        val flag =
+            if (disable) {
                 PackageManager.COMPONENT_ENABLED_STATE_DISABLED
             } else {
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED
             }
 
-            for (pkg in pkgs) {
-                if (isInstalled(pm, pkg)) {
-                    pm.setApplicationEnabledSetting(pkg, flag, 0)
-                }
+        for (pkg in TELEPHONY_PACKAGES) {
+            if (isInstalled(pm, pkg)) {
+                pm.setApplicationEnabledSetting(pkg, flag, 0)
             }
         }
     }

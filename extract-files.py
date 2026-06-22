8b1e7beb72d339f8b6fa62c51f50ae24133ed027
Michael Bestas <mkbestas@lineageos.org>
2026-06-22 03:46:06 +0300

    nicole: Update WFD system blobs from marvel_g A171WEH.20
    Change-Id: I733c2bf342175474ce7f02d467a17ded6f0a8760

--- extract-files.py
+++ extract-files.py
@@ -45,8 +45,10 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'product/etc/permissions/razer_apnlib.xml': blob_fixup()
         .regex_replace('system', 'product'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libinput_shim.so'),
+    'system/framework/WfdCommon.jar': blob_fixup()
+        .apktool_patch('blob-patches/WfdCommon.patch'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/etc/init/android.hardware.radio@1.4-service.quectel.rc': blob_fixup()
         .regex_replace(' quectel.ril-daemon', ' vendor.quectel.ril-daemon')
         .add_line_if_missing('    disabled'),

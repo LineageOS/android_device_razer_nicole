5082ffff0ab608934650352e54d8add161d438d1
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:36:27 -0400

    nicole: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,6 +159,7 @@ BOOT_SECURITY_PATCH := 2023-02-01
 VENDOR_SECURITY_PATCH := 2023-02-01
 
 # SEPolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
--- device.mk
+++ device.mk
@@ -186,6 +186,9 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml
 
+# libion
+$(call soong_config_set_bool,libion,enabled,true)
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
